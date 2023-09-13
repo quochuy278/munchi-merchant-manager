13cf69afd3b7f5b379a3fa01c9ca9e00c201a0cc
Huy Bui <quochuy270800@gmail.com>
2023-09-13 18:54:43 +0300

    Add Order Board Page

---
+++ src/pages/OrderBoardPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DUMMY_DATA } from "../data";
+import Layout from "../components/Layout";
+
+const OrderBoardPage = () => {
+  console.log(DUMMY_DATA);
+  return (
+    <Layout>
+      <div>OrderBoardPage</div>
+    </Layout>
+  );
+};
+
+export default OrderBoardPage;
