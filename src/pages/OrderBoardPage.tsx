4939b3490fefd49cb2524456fb15e0df0588c12e
Huy Bui <quochuy270800@gmail.com>
2023-10-03 13:39:40 +0300

    remove layout in order board page

--- src/pages/OrderBoardPage.tsx
+++ src/pages/OrderBoardPage.tsx
@@ -1,14 +1,11 @@
 import { Box } from "@mui/material";
-import Layout from "../components/Layout";
 import OrderBoard from "../features/order/OrderBoard";
 
 const OrderBoardPage = () => {
   return (
-    <Layout>
       <Box height={"calc(100vh - 64px)"}>
         <OrderBoard />
       </Box>
-    </Layout>
   );
 };
 
