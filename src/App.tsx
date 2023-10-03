b010045aa34e976c91212adefbc0ac8469504cb6
Huy Bui <quochuy270800@gmail.com>
2023-10-03 13:37:57 +0300

    add business route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import OrderBoardPage from "./pages/OrderBoardPage";
+import BusinessPage from "./pages/BusinessPage";
+import Layout from "./components/Layout";
+import { BusinessHeader, OrderHeader } from "./components/Header";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<OrderBoardPage />} />
+      <Route
+        path="/"
+        element={
+          <Layout renderHeader={() => <OrderHeader />}>
+            <OrderBoardPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/business"
+        element={
+          <Layout renderHeader={() => <BusinessHeader />}>
+            <BusinessPage />
+          </Layout>
+        }
+      />
     </Routes>
   );
 }
